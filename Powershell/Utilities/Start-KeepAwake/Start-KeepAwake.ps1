75a068b66205700f95f5556938c72a1004463bd5
Jared Heinrichs <jared.heinrichs@gmail.com>
2024-09-24 09:39:24 -0500

    Add files via upload

---
+++ Powershell/Utilities/Start-KeepAwake/Start-KeepAwake.ps1
@@ -0,0 +1,68 @@
+<#
+=================================================================
+Start-KeepAwake
+Written By: Jared Heinrichs
+=================================================================
+#>
+
+param (
+        $Hours,
+        [switch]$Pause
+    )
+function Start-KeepAwake {
+    [CmdletBinding()]
+    param (
+        $Hours,
+        [switch]$Pause
+    )
+    
+    begin {
+        $wsh = New-Object -ComObject WScript.Shell
+        $EndDateTime = (Get-Date).addHours($Hours)
+        $StartDateTime = Get-Date
+    }
+    
+    process {
+        while ($StartDateTime -le $EndDateTime) {
+            # Send Shift+F15 - this is the least intrusive key combination
+            $wsh.SendKeys('+{F15}')
+            $RandomTime = Get-Random -Minimum 60 -Maximum 300
+            Start-Sleep -seconds $RandomTime
+            $StartDateTime = Get-Date
+        }
+    }
+    
+    end {
+        $Date = Get-Date
+        Write-Host "Utility Complete: $Date"
+        Write-Host
+        Write-Host
+        if ($Pause){
+            pause
+        }
+    }
+}
+
+<#
+=================================================================
+Start Keep Awake Utility
+=================================================================
+#>
+
+Write-Host
+Write-Host
+$CurrentDateTime = Get-Date
+$CurrentDateTime
+Write-Host
+
+#If there are no hours it most likely means that the person just double clicked the file to run.
+#We will automatically add a pause at the end of the time.
+if (!($Hours)){
+    $Hours = Read-Host "In how many --hours-- would you like to stop this utility?"
+    $Pause = $true
+}
+if ($Pause){
+    Start-KeepAwake -Hours $Hours
+} else {
+    Start-KeepAwake -Hours $Hours -Pause
+}
