a647e62c612050249bbf5bee2cf3e815d3a6a6b0
Jared Heinrichs <jared.heinrichs@gmail.com>
2024-10-26 00:37:58 -0500

    Add files via upload

---
+++ Powershell/Utilities/Get-MonitorInfo.ps1
@@ -0,0 +1,42 @@
+function Start-Decode {
+    If ($args[0] -is [System.Array]) {
+        [System.Text.Encoding]::ASCII.GetString($args[0])
+    }
+    Else {
+        "Not Found"
+    }
+}
+function Get-MonitorInfo {
+    
+    begin {
+        $Monitors = @()
+    }
+    
+    process {
+        $MonitorNum = 0
+        ForEach ($Monitor in Get-CimInstance WmiMonitorID -Namespace root\wmi) {
+            [int]$MonitorNum += 1
+            [string]$HardwareID = ($Monitor.InstanceName).split("\")[1]
+            [string]$FullName = Start-Decode $Monitor.UserFriendlyName -notmatch 0
+            $pos = $FullName.IndexOf(" ")[0]
+            [string]$Manufacturer = $FullName.Substring(0, $pos)
+            [string]$Model = $FullName.Substring($pos+1)
+            [string]$SerialNum = Start-Decode $Monitor.SerialNumberID -notmatch 0
+
+            $MonitorProperty = [ordered]@{
+                "Monitor Number" = $MonitorNum
+                Manufacturer = $Manufacturer.toupper()
+                "Model Name" = $Model
+                "Serial Number" = $SerialNum
+                "Hardware ID" = $HardwareID
+            }
+            $MonitorObj = New-Object -TypeName psobject -Property $MonitorProperty
+            $Monitors += $MonitorObj
+        }
+    }
+    
+    end {
+        $Monitors
+    }
+}
+Get-MonitorInfo
