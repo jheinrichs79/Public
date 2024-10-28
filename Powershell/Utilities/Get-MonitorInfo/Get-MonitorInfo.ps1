e81bf43f14adf039eac78fa25194374d53a15488
Jared Heinrichs <jared.heinrichs@gmail.com>
2024-10-27 21:14:33 -0500

    Update Get-MonitorInfo.ps1

--- Powershell/Utilities/Get-MonitorInfo/Get-MonitorInfo.ps1
+++ Powershell/Utilities/Get-MonitorInfo/Get-MonitorInfo.ps1
@@ -1,3 +1,34 @@
+<#
+.SYNOPSIS
+    This utility will display all the monitors connected to the system.
+    This does not work in a virtual environment or RDP.
+
+.DESCRIPTION
+    DisplaysMonitor Number
+    Primary Display
+    Manufacturer
+    Serial Number
+    Hardware ID
+    Width (Current)
+    Height (Current)
+    Orientation
+    Cable Connection
+
+.EXAMPLE
+    ===============================================================
+    $MonitorInfo = .\Get-MonitorInfo.ps1
+    ===============================================================
+    $MonitorInfo            #Displays all Monitors
+    $MonitorInfo[0]         #Displays only the first monitor
+    $MonitorInfo.count      #Shows the number of Monitors
+
+
+.NOTES
+    Written by: Jared Heinrichs
+    Blog: https://jaredheinrichs.substack.com/
+    Public GitHub: https://github.com/jheinrichs79/Public
+
+#>
 function Start-Decode {
     If ($args[0] -is [System.Array]) {
         [System.Text.Encoding]::ASCII.GetString($args[0])
@@ -6,9 +37,10 @@ function Start-Decode {
         "Not Found"
     }
 }
+
 function Get-Primary {
     [string]$primary = ([System.Windows.Forms.Screen]::PrimaryScreen).DeviceName
-    $primary.remove(0,($primary.length -1))
+    $primary.remove(0, ($primary.length - 1))
 }
 function Get-MonitorInfo {
 
@@ -19,36 +51,44 @@ function Get-MonitorInfo {
     process {
         $MonitorNum = 0
         ForEach ($Monitor in Get-CimInstance WmiMonitorID -Namespace root\wmi) {
-            $PrimaryMonitorNum = Get-Primary
+            
             [int]$MonitorNum += 1
+            $FormsInfo = [System.Windows.Forms.Screen]::AllScreens | Sort-Object DeviceName
             [string]$HardwareID = ($Monitor.InstanceName).split("\")[1]
             [string]$FullName = Start-Decode $Monitor.UserFriendlyName -notmatch 0
             $pos = $FullName.IndexOf(" ")[0]
             [string]$Manufacturer = $FullName.Substring(0, $pos)
-            [string]$Model = $FullName.Substring($pos+1)
+            [string]$Model = $FullName.Substring($pos + 1)
             [string]$SerialNum = Start-Decode $Monitor.SerialNumberID -notmatch 0
-
-            if ($PrimaryMonitorNum -eq $MonitorNum){
-                $MonitorProperty = [ordered]@{
-                    "Monitor Number" = $MonitorNum
-                    "Primary Display" = $true
-                    Manufacturer = $Manufacturer.toupper()
-                    "Model Name" = $Model
-                    "Serial Number" = $SerialNum
-                    "Hardware ID" = $HardwareID
-                }
+            if (($FormsInfo[$MonitorNum-1].Bounds.Width) -gt ($FormsInfo[$MonitorNum-1].Bounds.Height)){
+                $Orientation = "Horizontal"
             } else {
-                $MonitorProperty = [ordered]@{
-                    "Monitor Number" = $MonitorNum
-                    "Primary Display" = $false
-                    Manufacturer = $Manufacturer.toupper()
-                    "Model Name" = $Model
-                    "Serial Number" = $SerialNum
-                    "Hardware ID" = $HardwareID
-                }
+                $Orientation = "Vertical"
             }
-            
+            $CableTypeNum = ((Get-WmiObject WmiMonitorConnectionParams -Namespace root\wmi) | Where-Object InstanceName -like "*DELA09A*" | Select-Object VideoOutputTechnology).VideoOutputTechnology
+            if ($CableTypeNum -eq 10){
+                $CableType = "Display Port"
+            } elseif ($CableTypeNum -eq 5){
+                $CableType = "HDMI"
+            } elseif ($CableTypeNum -eq 4){
+                $CableType = "DVI"
+            } else {
+                $CableType = "Unknown"
+            }
+            $MonitorProperty = [ordered]@{
+                "Monitor Number"  = $MonitorNum
+                "Primary Display" = $FormsInfo[$MonitorNum-1].Primary
+                "Manufacturer"    = $Manufacturer.toupper()
+                #"Model Name"      = $Model
+                "Serial Number"   = $SerialNum
+                "Hardware ID"     = $HardwareID
+                "Width (Current)" = $FormsInfo[$MonitorNum-1].Bounds.Width
+                "Height (Current)"= $FormsInfo[$MonitorNum-1].Bounds.Height
+                "Orientation"     = $Orientation
+                "Cable Connection"= $CableType
+            }
             $MonitorObj = New-Object -TypeName psobject -Property $MonitorProperty
+            
             $Monitors += $MonitorObj
         }
     }
