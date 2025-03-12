509b10b04fa42e8d835598b8b29bebfdf07b196c
jared.heinrichs@gmail.com <jared.heinrichs@gmail.com>
2025-03-11 19:51:34 -0500

    commit

--- Powershell/Utilities/Start-SplashtopCleanup/Start-SplashtopCleanup.ps1
+++ Powershell/Utilities/Start-SplashtopCleanup/Start-SplashtopCleanup.ps1
@@ -1,4 +1,4 @@
-cd "$env:windir\Temp"
+Set-Location "$env:windir\Temp"
 Uninstall-Package -Name 'Splashtop Streamer' -Force
 MsiExec.exe /qn /X{B7C5EA94-B96A-41F5-BE95-25D78B486678}
 Remove-Item -Recurse -Force 'C:\Program Files (x86)\Splash*' -ErrorAction SilentlyContinue
