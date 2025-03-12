9ead8414bf936080eaf1439263090b5db29eb525
jared.heinrichs@gmail.com <jared.heinrichs@gmail.com>
2025-03-12 10:38:50 -0500

    commit

--- Powershell/Utilities/Teams/Start-TeamsLoginReset/Start-TeamsLoginReset.ps1
+++ Powershell/Utilities/Teams/Start-TeamsLoginReset/Start-TeamsLoginReset.ps1
@@ -20,9 +20,34 @@ function Test-Folders {
         return $FoldersThatExist
     }
 }
+function Close-Teams {
+    [CmdletBinding()]
+    param (
+        #Not Needed
+    )
+    
+    begin {
+        $teamsProcesses = Get-Process | Where-Object { $_.ProcessName -like "*teams*" }        
+    }
+    
+    process {
+        foreach ($process in $teamsProcesses) {
+            Write-Host "Terminating process: $($process.ProcessName) (ID: $($process.Id))"
+            Stop-Process -Id $process.Id -Force
+        }
+    }
+    
+    end {
+        Write-Host "All processes containing '*teams*' have been terminated."
+        Write-Host ""
+    }
+}
 function Start-TeamsLoginReset {
     [CmdletBinding()]
-   
+    param (
+        #Not Needed
+    )
+    
     begin {
         $RoamingProfile = $env:APPDATA
         $LocalProfile = $env:LOCALAPPDATA
@@ -43,15 +68,23 @@ function Start-TeamsLoginReset {
         Write-Output "Teams Login Reset"
         Write-Output "------------------"    
         Write-Output "Here are the folders that exist:"
+
         foreach ($Folder in $FoldersThatExist) {
             Write-Output $Folder
         }
+        
         Write-Output "Teams needs to be closed before removing these folders."
+        Write-Output ""
+        Write-Output ""
+
+        Close-Teams
+        
+        Write-Output ""
         Write-Output ""
         $answer = Read-Host "Would you like to remove these folders?(yes/no)"
         if ($answer -like "y*") {
             foreach ($Folder in $FoldersThatExist) {
-                Remove-Item -Path $Folder -Recurse -Force
+                Remove-Item -Path $Folder -Recurse -Force -ErrorAction SilentlyContinue
             }
         }
         else {
@@ -61,7 +94,6 @@ function Start-TeamsLoginReset {
     
     end {
         Write-Output "Your machine needs to be restarted for the changes to take effect."
-        pause
     }
 }
 
