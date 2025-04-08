b24e55498a572b462bc625ab499e84cba1d02440
jared.heinrichs@gmail.com <jared.heinrichs@gmail.com>
2025-04-08 09:53:45 -0500

    commit

--- Powershell/Get-MyPublicIPAddress/Get-MyPublicIPAddress.ps1
+++ Powershell/Get-MyPublicIPAddress/Get-MyPublicIPAddress.ps1
@@ -1,23 +1,23 @@
-$var = nslookup myip.opendns.com resolver1.opendns.com
-$var.Address.TypeNameOfValue
-
 function Get-MyPublicIPAddress {
     [CmdletBinding()]
     param (
-        $myip = "myip.opendns.com",
-        $resolver = "resolver1.opendns.com"
+        
     )
     
     begin {
+        $myip = "myip.opendns.com"
+        $resolver = "resolver1.opendns.com"
+        $var = nslookup myip.opendns.com resolver1.opendns.com
         Write-Host $myip
         Write-Host $resolver
     }
     
     process {
-        
+        $var = nslookup $myip $resolver
     }
     
     end {
-        
+        $var
     }
 }
+Get-MyPublicIPAddress
