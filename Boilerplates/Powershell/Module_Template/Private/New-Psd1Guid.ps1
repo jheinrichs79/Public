254b57cec92d14a2fa00807a91bfe298f21076f9
Jared Heinrichs <jared.heinrichs@gmail.com>
2026-01-27 07:44:14 -0600

    Add files via upload

---
+++ Boilerplates/Powershell/Module_Template/Private/New-Psd1Guid.ps1
@@ -0,0 +1,9 @@
+﻿function New-Psd1Guid {
+    [CmdletBinding()]
+    param()
+
+    # Generate a GUID and force lowercase
+    $Guid = [guid]::NewGuid().ToString().ToLower()
+
+    return $Guid
+}
---
+++ Boilerplates/Powershell/Module_Template/Private/Set-Placeholder.ps1
@@ -0,0 +1,31 @@
+﻿function Set-Placeholder {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [string]$Path,
+
+        [Parameter(Mandatory)]
+        [string]$Placeholder,
+
+        [Parameter(Mandatory)]
+        [AllowEmptyString()]
+        [string]$Replacement
+
+    )
+
+    if (-not (Test-Path $Path)) {
+        throw "File not found: $Path"
+    }
+
+    $content = Get-Content -Path $Path -Raw
+
+    if ([string]::IsNullOrEmpty($content)) {
+        throw "File is empty or unreadable: $Path"
+    }
+
+    $updated = $content.Replace($Placeholder, $Replacement)
+
+    Set-Content -Path $Path -Value $updated
+
+    #$updated
+}
